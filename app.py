e2be0ed228bbe2558078f3af97a13ebcaff57425
rickylin1 <pandanation33@gmail.com>
2024-05-31 17:16:31 -0700

    Added homepage frontend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for, session, redirect, jsonify
+from flask import Flask, request, url_for, session, redirect, jsonify, render_template
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
@@ -7,21 +7,43 @@ import time
 import json
 import csv
 
-#NOTE for some reason must do flask run, cannot just run 
-#next to-do is to add automatic refresh so that I don't have to automatically refresh everytime
-#also reduce inefficiencies by removing duplication?
-
 app = Flask(__name__)
 app.secret_key = "ONcs92894hfnl"
 app.config['SESSION_COOKIE_NAME'] = 'Rickys cookie'
 TOKEN_INFO = "token_info"
 
-# if __name__ == '__main__':
-#     app.run(debug = True)
-
 load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
+
+def get_spotify():
+    try:
+        token_info = get_token()
+    except:
+        print("User not logged in")
+        return None
+    return spotipy.Spotify(auth=token_info['access_token'])
+
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise "exception"
+
+    now = int(time.time())
+    expired = token_info['expires_at'] - now < 60
+    if expired:
+        sp_oauth = create_spotify_oath()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+    return token_info
+
+
+def create_spotify_oath():
+    return SpotifyOAuth(
+        client_id=client_id, 
+        client_secret= client_secret, 
+        redirect_uri= url_for('redirectPage', _external = True),
+         scope="user-library-read user-top-read user-read-currently-playing user-modify-playback-state playlist-modify-private" )
 
 def json_to_csv(json_data, csv_file_name):
     csv_file = csv_file_name
@@ -37,143 +59,65 @@ def json_to_csv(json_data, csv_file_name):
         for item in json_data:
             writer.writerow(item.values())
 
+def parse_query(query):
+    params = {}
+    # Check if the query contains "by", indicating it's in the format "track by artist"
+    if " by " in query:
+        # Split the query by "by" to separate track and artist
+        track, artist = query.split(" by ", 1)
+        params['track'] = track.strip()
+        params['artist'] = artist.strip()
+    else:
+        # Split the query into words
+        words = query.split()
+        modified_query = []
+        for word in words:
+            # Check if the word contains letters and digits (assuming digits are in artist names)
+            if any(char.isalpha() for char in word) and any(char.isdigit() for char in word):
+                # If it contains both letters and digits, assume it's in the format "track:Unwritten artist:Natasha Bedingfield"
+                modified_query.append(word)
+            else:
+                # Otherwise, keep the word as it is
+                modified_query.append(word)
+        # Join the modified query to form the final query string
+        query = ' '.join(modified_query)
+        # Split the modified query by spaces
+        parts = query.split()
+        # Iterate through each part to extract parameters
+        for part in parts:
+            # Split each part by ':'
+            key_value = part.split(':')
+            # Ensure there's a key-value pair
+            if len(key_value) == 2:
+                key, value = key_value
+                # Store the key-value pair in the params dictionary
+                params[key.lower()] = value
+        # If no 'track' key was found, assume the input is just the artist name
+        if 'track' not in params:
+            params['artist'] = ' '.join(parts)
+
+    return params
+
+# # Example usage:
+# query = 'Unwritten by Natasha Bedingfield'
+# search_params = parse_query(query)
+# print(search_params)
+
+# query = 'Natasha Bedingfield'
+# search_params = parse_query(query)
+# print(search_params)
+
+# {'track': 'Unwritten', 'artist': 'Natasha Bedingfield'}
+# {'artist': 'Natasha Bedingfield'}
 
 
 
-@app.route('/')
-def login():
-    sp_oauth = create_spotify_oath()
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
-
-@app.route('/redirect')
-def redirectPage():
-    sp_oauth = create_spotify_oath()
-    session.clear()
-    code = request.args.get('code')
-    access_token = sp_oauth.get_access_token(code)
-    session[TOKEN_INFO] = access_token
-    # return redirect(url_for('getTracks', _external = True))
-    # return redirect(url_for('addSongToQueue', _external = True))
-    # return redirect(url_for('audio_features', _external = True))
-    # return redirect(url_for('Pause', _external = True))
-    # return redirect(url_for('Resume', _external = True))
-    # return redirect(url_for('Previous', _external = True))
-    # return redirect(url_for('Next', _external = True))
-    # return redirect(url_for('CreatePlaylist', _external = True))
-    # return redirect(url_for('SimilarSongs', _external = True))
-    # return redirect(url_for('get20TopArtists', _external = True))
-    return redirect(url_for('get50TopTracks', _external = True))
-    ##for top tracks and artists can specify a time range as well
-
-    # return 'redirect'
-@app.route('/getTracks')
-def getTracks():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    current_track = sp.current_user_playing_track()
-    formatted_info = format_currently_playing_track(current_track)
-    return formatted_info
-
-
-@app.route('/Pause')
-def Pause():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    sp.pause_playback()
-    return 'paused'
-
-
-def AddSongToPlaylist(playlistid):
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    trackid, trackname = search_song('track:Unwritten artist:Natasha Bedingfield')
-    userprofile = sp.current_user()
-    userid = userprofile['id']
-    sp.user_playlist_add_tracks(user = userid, playlist_id=playlistid, tracks = [trackid])
-    return 'added songs'
-    
-
-
-@app.route('/CreatePlaylist')
-def CreatePlaylist():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    name = "a test playlist"
-    userprofile = sp.current_user()
-    userid = userprofile['id']
-    playlist = sp.user_playlist_create(user = userid, name = "a test", public = False)
-    id = playlist['id']
-    AddSongToPlaylist(id)
-
-    return 'created playlist'
-
-@app.route('/Resume')
-def Resume():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    sp.start_playback()
-    return 'start'
-
-@app.route('/Previous')
-def Previous():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    sp.previous_track()
-    return 'prev'
-
-
-@app.route('/Next')
-def Next():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    sp.next_track()
-    return 'next'
 
 #return song id given a query
 def search_song(query):
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
     result = sp.search(q = query, type = 'track', limit = 1)
     if result['tracks']['items']:
         track = result['tracks']['items'][0]
@@ -186,13 +130,9 @@ def search_song(query):
     
 #return artist id given a query
 def search_artist(query):
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
     result = sp.search(q = query, type = 'artist', limit = 1)
     if result['artists']['items']:
         artist = result['artists']['items'][0]
@@ -204,39 +144,17 @@ def search_artist(query):
         return "No results found"
 
 def get_album_id(query):
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
     results = sp.search(q=query, limit=1, type='album')
-    print('test')
-    print('test')
-    print('test')
-    print(str(type(results)))
-    print(results['albums']['items'][0]['id'])
-
-    # album_list = []
-    # for album in results['albums']['items']:
-    #     album_info = {
-    #         'name': album['name'],
-    #         'artist': album['artists'][0]['name'],
-    #         'release_date': album['release_date']
-    #         # Add more fields as needed
-    #     }
-    #     album_list.append(album_info)
     
     return results['albums']['items'][0]['id']
 
 def get_album_tracks(album_id):
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
     tracks = sp.album_tracks(album_id)
     return tracks
 
@@ -245,63 +163,138 @@ def get_album_tracks(album_id):
 # def NewReleases():
 
 
+#recommendation
+
+#remove old songs
+
+#get playlists
+
+#find duplicates
+
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/login')
+def login():
+    sp_oauth = create_spotify_oath()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+@app.route('/redirect')
+def redirectPage():
+    sp_oauth = create_spotify_oath()
+    session.clear()
+    code = request.args.get('code')
+    access_token = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = access_token
+    return redirect(url_for('getCurrentTrack', _external = True))
+
+@app.route('/getCurrentTrack')
+def getCurrentTrack():
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
+    current_track = sp.current_user_playing_track()
+    formatted_info = format_currently_playing_track(current_track)
+    return formatted_info
+
+
+@app.route('/Pause')
+def Pause():
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
+    sp.pause_playback()
+    return 'paused'
+
+
+def AddSongToPlaylist(playlistid):
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
+    trackid, trackname = search_song('track:Unwritten artist:Natasha Bedingfield')
+    userprofile = sp.current_user()
+    userid = userprofile['id']
+    sp.user_playlist_add_tracks(user = userid, playlist_id=playlistid, tracks = [trackid])
+    return 'added songs'
+    
+@app.route('/CreatePlaylist')
+def CreatePlaylist():
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
+    name = "a test playlist"
+    userprofile = sp.current_user()
+    userid = userprofile['id']
+    playlist = sp.user_playlist_create(user = userid, name = "a test", public = False)
+    id = playlist['id']
+    AddSongToPlaylist(id)
+
+    return 'created playlist'
+
+@app.route('/Resume')
+def Resume():
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
+    sp.start_playback()
+    return 'start'
+
+@app.route('/Previous')
+def Previous():
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
+    sp.previous_track()
+    return 'prev'
+
+
+@app.route('/Next')
+def Next():
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
+    sp.next_track()
+    return 'next'
+
 @app.route('/addSongToQueue')
 def addSongToQueue():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    # songid, songname = search_song('track:Unwritten artist:Natasha Bedingfield')
-    songid, songname = search_song('Unwritten Natasha Bedingfield')
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
+    songid, songname = search_song('track:Unwritten artist:Natasha Bedingfield')
     sp.add_to_queue(uri = songid)
     return f"Song: {songname} has been added to the queue."
 
 @app.route('/SimilarSongs')
 def SimilarSongs():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
     artist_id, artist_name= search_artist(query = 'artist:aespa')
     related_artists = sp.artist_related_artists(artist_id)
     return f"related artists to {artist_name} are {related_artists}"
 
 @app.route('/audio_analysis')
 def audio_analysis():
-    try:
-        token_info = get_token()
-    except:
-        print("User not logged in")
+    sp = get_spotify()
+    if sp is None:
         return redirect(url_for("login", _external=True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    
     song_id, song_name = search_song('track:Trap Queen artist:Fetty Wap')
-
     sp.add_to_queue(uri = song_id)
-    
-
     analysis = sp.audio_analysis(song_id)
-    
+
 
     return analysis
 
 
 @app.route('/audio_features')
 def audio_features():
-    try:
-        token_info = get_token()
-    except:
-        print("User not logged in")
+    sp = get_spotify()
+    if sp is None:
         return redirect(url_for("login", _external=True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
     album_id = get_album_id('album%3ARodeo%20artist%3ATravis%20Scott')
     album = get_album_tracks(album_id)
     print('yoyo')
@@ -318,29 +311,32 @@ def audio_features():
 
     return analysis
 
-@app.route('/get20TopArtists')
-def get20TopArtists():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+#time frames(short_term 4 weeks, medium_term 6 months default, long_term roughly 1 year
+@app.route('/get50TopArtists')
+def get50TopArtists():
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
     top_artists = sp.current_user_top_artists(limit=50, offset = 0)['items'][0:50]
-    format_top_artists(top_artists)
-    json_to_csv(top_artists, "./data/topArtists.csv")
-    return "hi"
+    formatted_artists = []
+    for artist in top_artists:
+        image_data = artist.get('images', [])  # Get the list of images or an empty list if not available
+        image_url = image_data[0]['url'] if image_data else None
+        formatted_artist = {
+            'name': artist['name'],
+            'genres': artist['genres'],
+            'image_url': image_url  # Include the image URL in the formatted data
+        }
+        formatted_artists.append(formatted_artist)
+    
+    json_to_csv(top_artists, "./data/topTracks.csv")
+    return jsonify(formatted_artists)
 
 @app.route('/get50TopTracks')
 def get50TopTracks():
-    try:
-        token_info = get_token()
-    except:
-        print("user not logged in")
-        return redirect(url_for("login", _external = True))
-
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+    sp = get_spotify()
+    if sp is None:
+        return redirect(url_for("login", _external=True))
     top_tracks = sp.current_user_top_tracks(limit=50, offset=0)['items']
     formatted_tracks = []
     for track in top_tracks:
@@ -371,12 +367,6 @@ def format_top_artists(top_artists):
 
 # Function to format and print the currently playing track information
 def format_currently_playing_track(track_info):
-    print('test')
-    print('test')
-    print('test')
-    print('test')
-    print('test')
-    print('test')
     if not track_info or not track_info.get('item'):
         return "No track is currently playing."
 
@@ -410,39 +400,6 @@ def format_currently_playing_track(track_info):
     print(formatted_info)
     return formatted_info
 
-# @app.route('/getPlaylists')
-# def getPlaylists():
-#     try:
-#         token_info = get_token()
-#     except:
-#         print("user not logged in")
-#         return redirect(url_for("login", _external = False))
-
-#     sp = spotipy.Spotify(auth=token_info['access_token'])
-#     return str(sp.playlisto(limit=20, offset = 0)['items'][0:5])
-
-
-
-def get_token():
-    token_info = session.get(TOKEN_INFO, None)
-    if not token_info:
-        raise "exception"
-
-    now = int(time.time())
-    expired = token_info['expires_at'] - now < 60
-    if expired:
-        sp_oauth = create_spotify_oath()
-        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-    
-    return token_info
-
-
-def create_spotify_oath():
-    return SpotifyOAuth(
-        client_id=client_id, 
-        client_secret= client_secret, 
-        redirect_uri= url_for('redirectPage', _external = True),
-         scope="user-library-read user-top-read user-read-currently-playing user-modify-playback-state playlist-modify-private" )
 
 
 if __name__ == '__main__':
